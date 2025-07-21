db121e3bc75a526721baa156e5e267bdcfaae7dc
Sergii <s.ignatov92@gmail.com>
2025-07-21 22:38:44 +0300

    Refactored form to reusable components

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,4 +1,7 @@
 import {useState} from "react";
+import SingleLineText from "./form-components/SingleLineText.jsx";
+import TextArea from "./form-components/TextArea.jsx";
+import SelectList from "./form-components/SelectList.jsx";
 
 const NoteForm = ({notes, setNotes}) => {
 
@@ -47,49 +50,42 @@ const NoteForm = ({notes, setNotes}) => {
             {/*form*/}
             {isFormVisible && (
                 <form onSubmit={handleSubmit} className={'mb-6'}>
-                    <div className="mb-4">
-                        <label htmlFor="title" className="block font-semibold">Title</label>
-                        <input
-                            name="title"
-                            type="text"
-                            className="w-full p-2 border rounded-lg"
-                            value={formData.title}
-                            onChange={handleChange}/>
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="priority" className="block font-semibold">Priority</label>
-                        <select
-                            name="priority"
-                            className="w-full p-2 border rounded-lg"
-                            value={formData.priority}
-                            onChange={handleChange}>
-                            <option value="High">🔴 High</option>
-                            <option value="Medium">🟡 Medium</option>
-                            <option value="Low">🟢 Low</option>
-                        </select>
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="category" className="block font-semibold">Category</label>
-                        <select
-                            name="category"
-                            className="w-full p-2 border rounded-lg"
-                            value={formData.category}
-                            onChange={handleChange}>
-                            <option value="Work">💼 Work</option>
-                            <option value="Study">📚 Study</option>
-                            <option value="Personal">📃Personal</option>
-                            <option value="Ideas">💡 Ideas</option>
-                        </select>
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="description" className="block font-semibold">Description</label>
-                        <textarea
-                            name="description"
-                            className="w-full p-2 border rounded-lg"
-                            value={formData.description}
-                            onChange={handleChange}>
-                </textarea>
-                    </div>
+                    <SingleLineText
+                        label="Title"
+                        name="title"
+                        value={formData.title}
+                        onChange={handleChange}
+                        required={true}
+                    />
+                    <SelectList label="Priority"
+                                name="priority"
+                                value={formData.priority}
+                                onChange={handleChange}
+                                options={[
+                                    {value: 'Medium', label: ' 🟡 Medium'},
+                                    {value: 'Low', label: '🟢 Low'},
+                                    {value: 'High', label: '🔴 High'}
+                                ]}
+                    />
+                    <SelectList
+                        label="Category"
+                        name="category"
+                        value={formData.category}
+                        onChange={handleChange}
+                        options={[
+                            {value: "Work", label: '💼 Work'},
+                            {value: "Study", label: '📚 Study'},
+                            {value: "Personal", label: '📃 Personal'},
+                            {value: "Ideas", label: '💡 Ideas'},
+                        ]}
+                    />
+                    <TextArea
+                        label="Description"
+                        name="description"
+                        value={formData.description}
+                        onChange={handleChange}
+                        required
+                    />
                     <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600">Add Note</button>
                 </form>
             )}
