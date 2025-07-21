ca3fd9c45ff6bcaecf5f5a55fd8d430716eba36d
sergii.ignatov <s.ignatov92@gmail.com>
2025-07-21 15:58:41 +0300

    Refactored useStates into single one for NoteForm.jsx

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,28 +1,41 @@
 import {useState} from "react";
 
 const NoteForm = () => {
-    const [title, setTitle] = useState('');
-    const [priority, setPriority] = useState('Medium');
-    const [category, setCategory] = useState('Work');
-    const [description, setDescription] = useState('');
+    // const [title, setTitle] = useState('');
+    // const [priority, setPriority] = useState('Medium');
+    // const [category, setCategory] = useState('Work');
+    // const [description, setDescription] = useState('');
+
+    const [formData, setFormData] = useState({
+        title: '',
+        category: 'Work',
+        priority: 'Medium',
+        description: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
 
     return (
         <form className={'mb-6'}>
             <div className="mb-4">
                 <label htmlFor="title" className="block font-semibold">Title</label>
                 <input
+                    name="title"
                     type="text"
                     className="w-full p-2 border rounded-lg"
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
+                    value={formData.title}
+                    onChange={handleChange}
                 />
             </div>
             <div className="mb-4">
                 <label htmlFor="priority" className="block font-semibold">Priority</label>
                 <select
+                    name="priority"
                     className="w-full p-2 border rounded-lg"
-                    value={priority}
-                    onChange={e => setPriority(e.target.value)}
+                    value={formData.priority}
+                    onChange={handleChange}
                 >
                     <option value="High">🔴 High</option>
                     <option value="Medium">🟡 Medium</option>
@@ -32,10 +45,11 @@ const NoteForm = () => {
             <div className="mb-4">
                 <label htmlFor="category" className="block font-semibold">Category</label>
                 <select
+                    name="category"
                     className="w-full p-2 border rounded-lg"
-                    value={category}
-                    onChange={e => setCategory(e.target.value)}
-                >
+                    value={formData.category}
+                    onChange={handleChange}
+                    >
                     <option value="Work">💼 Work</option>
                     <option value="Study">📚 Study</option>
                     <option value="Personal">📃Personal</option>
@@ -45,9 +59,10 @@ const NoteForm = () => {
             <div className="mb-4">
                 <label htmlFor="description" className="block font-semibold">Description</label>
                 <textarea
+                    name="description"
                     className="w-full p-2 border rounded-lg"
-                    value={description}
-                    onChange={e => setDescription(e.target.value)}
+                    value={formData.description}
+                    onChange={handleChange}
                 ></textarea>
             </div>
             <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600">Add Note</button>
