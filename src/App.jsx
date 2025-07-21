00d7ab5d421094011afeb657ab968dc89144ca8d
Sergii <s.ignatov92@gmail.com>
2025-07-21 21:37:09 +0300

    added notes rendering under the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import NoteForm from "./components/NoteForm.jsx";
+import NoteList from "./components/NoteList.jsx";
 
 const App = () => {
     const [notes, setNotes] = useState([]);
@@ -10,6 +11,7 @@ const App = () => {
             <h2 className="text-2xl font-bold mb-4 text-center">📝 Notes App</h2>
 
             <NoteForm notes={notes} setNotes={setNotes} />
+            <NoteList notes={notes} />
         </div>
     );
 }
