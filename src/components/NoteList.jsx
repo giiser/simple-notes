f4a25fd585fa2b5b6f305f4e24e8ababd65d34b4
sergii.ignatov <s.ignatov92@gmail.com>
2025-07-22 14:28:19 +0300

    Refactored NoteList so that Note is a separate component

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,3 +1,5 @@
+import Note from "./Note.jsx";
+
 const NoteList = ({notes, deleteNote}) => {
 
     if(notes.length === 0){
@@ -11,28 +13,7 @@ const NoteList = ({notes, deleteNote}) => {
     return (
         <div className="space-y-4">
             {notes.map((note) => (
-                <div
-                    key={note.id}
-                    className={`p-4 bg-white rounded-lg shadow-md border-l-4 ${(note.priority) === 'High'? 'border-red-500': note.priority === 'Medium'? 'border-yellow-400': 'border-green-600'}`}>
-                    <h3 className="text-lg font-bold">{note.title}</h3>
-                    <p className="text-sm text-gray-600">
-                        <strong>Category: </strong>{note.category}
-                    </p>
-                    <p className="text-sm text-gray-600">
-                        <strong>Priority: </strong>{note.priority}
-                    </p>
-                    <p className="mt-2">
-                        <strong>Description: </strong>{note.description}
-                    </p>
-
-                    <button
-                        onClick={() => {
-                            deleteNote(note.id);
-                        }}
-                        className="mt-3 text-red-500 cursor-pointer transition hover:text-red-700">
-                        🗑️ Delete
-                    </button>
-                </div>
+                <Note key={note.id} note={note} deleteNote={deleteNote} />
             ))}
         </div>
     );
